ab5ce282bf5695cf5a517295bcf37d8ae5159b4a
Louis <louislam617@gmail.com>
2025-02-17 16:00:16 +0000

    update training links

--- src/Training/EnergyServices.js
+++ src/Training/EnergyServices.js
@@ -12,35 +12,35 @@ const EnergyServices = () => {
           <h1 className='training-title'>Energy Services</h1>
                    <div className="training-grid">
                <div className="training-item">
-                     <a href="https://drive.google.com/file/d/1xkuuP-4TZyRUlRmDvmY8m9y9ri7xmGau/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                     <a href="https://yourdwm.co.uk/wp-content/uploads/2025/02/Scottish-Power-Sales-training.-Picth-only.pdf" target="_blank" rel="noopener noreferrer">
                          <img src={Training1} alt="Training PowerPoint 1" className="training-thumbnail" />
                      </a>
                     <p className="training-description">Sales Pitch Training</p>
                  </div>
 
                 <div className="training-item">
-                    <a href="https://drive.google.com/file/d/1rxC6XyILcXJtcfjevfjV6c2PLXsXVWqw/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                    <a href="https://yourdwm.co.uk/wp-content/uploads/2025/02/SP-ESMS-training-18.07.24.pdf" target="_blank" rel="noopener noreferrer">
                          <img src={Training2} alt="Training PowerPoint 2" className="training-thumbnail" />
                      </a>
                     <p className="training-description">Getting Appliance into your pitch</p>
                 </div>
 
                 <div className="training-item">
-                    <a href="https://drive.google.com/file/d/1B3JwH09s_J9S9aTLVUTgoF_k6sTmXvnP/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://yourdwm.co.uk/wp-content/uploads/2025/02/DirectDebt-training.pdf" target="_blank" rel="noopener noreferrer">
                          <img src={Training3} alt="Training PowerPoint 2" className="training-thumbnail" />
                      </a>
                     <p className="training-description">Understanding Direct Debts</p>
                 </div>
 
                 <div className="training-item">
-                    <a href="https://drive.google.com/file/d/1Uq_gaU6S6G-e029aUGZZQDkGgEdgI2Gw/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                    <a href="https://yourdwm.co.uk/wp-content/uploads/2025/02/DWM-PSR-brief.-PDF.pdf" target="_blank" rel="noopener noreferrer">
                          <img src={PSR} alt="PSR" className="training-thumbnail" />
                      </a>
                     <p className="training-description">PSR Training</p>
                 </div>
 
                 <div className="training-item">
-                    <a href="https://drive.google.com/file/d/1T4rQSnlSpS5ZEcm9ahLYDpRDptFVE1PJ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://yourdwm.co.uk/wp-content/uploads/2025/02/DWM-Warm-Home-Discount-brief-1.pdf" target="_blank" rel="noopener noreferrer">
                          <img src={warmHome} alt="warmHome" className="training-thumbnail" />
                      </a>
                     <p className="training-description">The Warm Home Discount</p>
